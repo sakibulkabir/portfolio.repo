09ccb1975498d0b1fa805abf5e6615687fb3473f
Sakibul Kabir Risat <113694977+sakibulkabir@users.noreply.github.com>
2022-12-06 21:42:18 +0600

    first commit

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings:["","Web Developer", "web Desinger", "Frontend Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typed = new Typed(".name", {
+    strings:["","SAKIBUL", "KABIR", "RISAT"],
+    typeSpeed:100,
+    BackSpeed:5,
+    loop:true
+})
